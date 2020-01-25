f47e29ae1c7a0d531ab9cd477feda39833c0590b
yuxiang660 <yuxiang660@163.com>
2020-01-25 23:28:20 +0800

    Clean file component

--- src/components/files/index.js
+++ src/components/files/index.js
@@ -1,14 +1,9 @@
 import React from "react";
 
-import Layout from '../layout';
-import Sidebar from '../sidebar';
-
 export default class Files extends React.Component {
   render() {
     const edges = this.props.metadata;
     return (
-      <Layout>
-        <Sidebar />
         <div>
           <h1>All Pages</h1>
           <table>
@@ -26,7 +21,6 @@ export default class Files extends React.Component {
             </tbody>
           </table>
         </div>
-      </Layout>
     )
   }
 }
--- src/pages/debug.js
+++ src/pages/debug.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 import useMarkdownRemark from '../query/markdown-remark'
 import Files from '../components/files'
+import Layout from '../components/layout';
+import Sidebar from '../components/sidebar';
 
 export default props => {
   const metadata = useMarkdownRemark();
-  return <Files metadata={metadata}{...props} />
+  return (
+    <Layout>
+      <Sidebar />
+      <Files metadata={metadata}{...props} />
+    </Layout>
+  )
 }
