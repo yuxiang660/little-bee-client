8d9cb99a8d0c2d36d6272ece76c3c998833fb823
yuxiang660 <yuxiang660@163.com>
2020-02-06 19:00:07 +0800

    Add eslint task and update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,57 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:flowtype/recommended"
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/jsx-wrap-multilines': 0,
+    // Makes no sense for props validation error when it has already defined React.FC type.
+    'react/prop-types': 0,
+    'react/forbid-prop-types': 0,
+    'react/sort-comp': 1,
+    'react/jsx-one-expression-per-line': 0,
+    'react/jsx-filename-extension': 'off',
+    // Makes no sense to allow type inference for expression parameters, but require typing the response
+    '@typescript-eslint/explicit-function-return-type': [
+      'off',
+      { allowTypedFunctionExpressions: true },
     ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      { functions: false, classes: true, variables: true, typedefs: true },
     ],
-    "rules": {
-    },
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
-    }
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/explicit-member-accessibility': 0,
+    '@typescript-eslint/interface-name-prefix': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
+
+    // Conflict with prettier
+    'arrow-body-style': ['error', 'as-needed'],
+    'arrow-parens': 0,
+    'object-curly-newline': 0,
+    'implicit-arrow-linebreak': 0,
+    'operator-linebreak': 0,
+    'no-param-reassign': 1,
+    'space-before-function-paren': 0,
+    'import/extensions': 0,
+  },
 };
