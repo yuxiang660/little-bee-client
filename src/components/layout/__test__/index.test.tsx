6464087b2c391fd5737df89aa5d559b83bf75931
yuxiang660 <yuxiang660@163.com>
2020-02-07 19:49:56 +0800

    Add Layout test

---
+++ src/components/layout/__test__/index.test.tsx
@@ -0,0 +1,34 @@
+import Layout from '../index';
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { RenderCallback } from 'src/shared/types';
+
+import { useStaticQuery, StaticQuery } from 'gatsby';
+import siteMetadata from 'configs/jest/__fixtures__/site-metadata';
+
+const mockedStaticQuery = StaticQuery as jest.Mock<StaticQuery>;
+const mockedUseStaticQuery = useStaticQuery as jest.Mock<typeof useStaticQuery>;
+
+describe('Layout', () => {
+  const props = {
+    ...siteMetadata,
+    children: 'test',
+    description: 'test',
+    title: 'test',
+  };
+
+  beforeEach(() => {
+    mockedStaticQuery.mockImplementationOnce(
+      ({ render }: RenderCallback) => render(props),
+      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+      // @ts-ignore
+      mockedUseStaticQuery.mockReturnValue(props),
+    );
+  });
+
+  it('renders correctly', () => {
+    const tree = renderer.create(<Layout {...props} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
